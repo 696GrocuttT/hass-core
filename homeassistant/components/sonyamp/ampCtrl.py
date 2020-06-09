899df91f595a1ee6c18c001c4003542d40ed9adf
Thomas Grocutt <tom@dsoworld.co.uk>
2020-06-09 20:30:20 +0100

    Improvements to ampCtrl command line

--- homeassistant/components/sonyamp/ampCtrl.py
+++ homeassistant/components/sonyamp/ampCtrl.py
@@ -2,7 +2,7 @@
 
 import serial
 import time
-import Queue
+import queue
 import threading
 import argparse
 
@@ -16,7 +16,7 @@ OK_RESP                 = 0xFD
 # commands and values for the DSP functions, No zone is specified for these commands
 PDC_SOUND_ADAPTOR       = 0xA3
 SF_SEL_CMD              = 0x42
-SF_SEL_CMD_2CH_ST       = 0x0              
+SF_SEL_CMD_2CH_ST       = 0x0
 SF_SEL_CMD_A_DIRECT     = 0x2
 SF_SEL_CMD_AFD_AUTO     = 0x21
 SF_SEL_CMD_ESURROUND    = 0x32
@@ -41,15 +41,15 @@ INPUT_SEL_CMD_5p1_IN    = 0x20
 INPUT_SEL_CMD_ATV_HDMI  = 0x21
 INPUT_SEL_CMD_BD_HDMI   = 0x22
 TRIG_12V_CMD            = 0x43
-MEM_WRITE               = 0xC1 
-MEM_READ                = 0xC2 
+MEM_WRITE               = 0xC1
+MEM_READ                = 0xC2
 GUI_SET                 = 0x6A
 GUI_SET_GUI_TOGGLE      = 0x2A
 GUI_SET_CURSOR_UP       = 0x30
 GUI_SET_CURSOR_DOWN     = 0x31
-GUI_SET_CURSOR_RIGHT    = 0x32 
-GUI_SET_CURSOR_LEFT     = 0x33 
-GUI_SET_ENTER           = 0x34 
+GUI_SET_CURSOR_RIGHT    = 0x32
+GUI_SET_CURSOR_LEFT     = 0x33
+GUI_SET_ENTER           = 0x34
 # Memory mapped settings
 SYSTEM_ADDR_SPACE       = 2
 SPEAKER_SEL_ADDR        = 575
@@ -69,7 +69,7 @@ class AmpCmd:
         self.value   = value
         self.expResp = expResp
         self.zones   = zones
-  
+
 
     @staticmethod
     def AmpMemCmd(addr, value):
@@ -77,14 +77,14 @@ class AmpCmd:
         return AmpCmd(PDC_AMP, MEM_WRITE, data)
 
 
-    def __eq__(self, other): 
+    def __eq__(self, other):
         eq = False
         if isinstance(other, AmpCmd):
             eq = (self.pdc     == other.pdc)     and \
                  (self.cmd     == other.cmd)     and \
                  (self.value   == other.value)   and \
                  (self.expResp == other.expResp) and \
-                 (self.zones   == other.zones) 
+                 (self.zones   == other.zones)
         return eq
 
 
@@ -131,7 +131,7 @@ cmdAmpSpkB           = AmpCmd.AmpMemCmd(SPEAKER_SEL_ADDR, SPEAKER_SEL_B)
 cmdAmpSpkAB          = AmpCmd.AmpMemCmd(SPEAKER_SEL_ADDR, SPEAKER_SEL_AB)
 cmdAmpNightOff       = AmpCmd.AmpMemCmd(NIGHT_MODE, 0)
 cmdAmpNightOn        = AmpCmd.AmpMemCmd(NIGHT_MODE, 1)
-    
+
 
 
 ################################################################################
@@ -143,18 +143,18 @@ class AmpCtrl(threading.Thread):
         return (256 - checkSum) & 0xFF
 
 
-    def cmdGen(self, data):    
-        cmd = '\x02' + chr(len(data))
+    def cmdGen(self, data):
+        cmd = b'\x02' + len(data).to_bytes(1, byteorder="little")
         for x in data:
-            cmd = cmd + chr(x)
-        return cmd + chr(self.genCrc(data))
-    
-    
+            cmd = cmd + x.to_bytes(1, byteorder="little")
+        return cmd + self.genCrc(data).to_bytes(1, byteorder="little")
+
+
     def sendRawCommand(self, verbose, data, expectedResp=[]):
         cmdData = self.cmdGen(data)
         if verbose:
-            print ":".join("{0:x}".format(ord(c)) for c in cmdData)        
-        # keep trying to write out the command until we get the expected 
+            print(":".join("{0:x}".format(c) for c in cmdData))
+        # keep trying to write out the command until we get the expected
         # responce
         for i in range(RETRY_LIMIT):
             self.amp.write(cmdData)
@@ -163,23 +163,23 @@ class AmpCtrl(threading.Thread):
             expectedRespLen = len(expectedResp)
             if expectedRespLen != 0:
                 resp = self.amp.read(expectedRespLen)
-                ok = (''.join(chr(c) for c in expectedResp)) == resp
+                ok   = bytes(expectedResp) == resp
                 if self.verbose and not ok:
-                    print "invalid responce " + (":".join("{0:x}".format(ord(c)) for c in resp))
-            # if everything is ok then get out of this retry loop now, if 
-            # not then we sleep for a bit to let any other responce data 
-            # come back and then flush it as it's not what we excepted and 
+                    print("invalid responce " + (":".join("{0:x}".format(c) for c in resp)))
+            # if everything is ok then get out of this retry loop now, if
+            # not then we sleep for a bit to let any other responce data
+            # come back and then flush it as it's not what we excepted and
             # we don't know its length. This leaves the buffer clean for the
             # next try.
-            if ok: 
+            if ok:
                 break
             else:
                 time.sleep(CMD_FAIL_RECOVERY_TIME)
                 self.amp.flushInput()
-    
+
 
     def recieveData(self, pdc, cmd, value):
-        # keep trying to write out the command until we get the expected 
+        # keep trying to write out the command until we get the expected
         # responce
         retData = []
         for i in range(RETRY_LIMIT):
@@ -189,22 +189,22 @@ class AmpCtrl(threading.Thread):
             resp   = self.amp.read(2)
             ok     = len(resp) == 2
             if ok:
-                ok     = ok and (resp[0] == '\x02')
-                length = ord(resp[1])
-            # now read in the rest of the message now that we have the length 
+                ok     = ok and (resp[0] == 2)
+                length = resp[1]
+            # now read in the rest of the message now that we have the length
             # from the header
             if ok:
                 resp = self.amp.read(length + 1)
                 data = []
                 for c in resp:
-                    data.append(ord(c))
-                # get the CRC    
+                    data.append(c)
+                # get the CRC
                 ok = length == (len(data) - 1)
                 if ok:
                     retData = data[:length]
                     ok = self.genCrc(retData) == data[length]
 
-            # if still ok break out of the retry look, otherwise wait for a bit, 
+            # if still ok break out of the retry look, otherwise wait for a bit,
             # flush the buffer and try again.
             if ok:
                 break
@@ -213,7 +213,7 @@ class AmpCtrl(threading.Thread):
                 self.amp.flushInput()
         return retData
 
-    
+
     def sendZoneCommands(self, verbose, pdc, cmd, value, zones=1, expectedResp=[]):
         # send the commands for all requested zones
         for zone in range(NUM_ZONES):
@@ -222,34 +222,34 @@ class AmpCtrl(threading.Thread):
                 data = [pdc, cmd, zone]
                 data.extend(value)
                 self.sendRawCommand(verbose, data, expectedResp)
-    
-    
+
+
     def sendCommand(self, verbose, pdc, cmd, value, expectedResp=[]):
         # calculate the command to send
         data = [pdc, cmd]
         data.extend(value)
         self.sendRawCommand(verbose, data, expectedResp)
 
-    
+
     def pollStatus(self):
         # get the data for all zones
         for curZone in range(0, NUM_ZONES):
             data = self.recieveData(PDC_AMP, STATUS_REQ, [curZone])
             if (len(data) == 7) and (data[2] == curZone):
                 # Do we have valid data to compare against
-                newPwr = data[5] & 1                     
+                newPwr = data[5] & 1
                 if self.statusDataValid[curZone]:
-                    prevPwr = self.prevStatData[curZone][5] & 1 
+                    prevPwr = self.prevStatData[curZone][5] & 1
                     sendCmd = newPwr != prevPwr
                 else:
-                    sendCmd = True   
+                    sendCmd = True
                 if sendCmd:
                     cmd = AmpCmd(PDC_AMP, PWR_CTRL_CMD, [newPwr], [OK_RESP], 1 << curZone)
                     self.rxCmdQueue.put(cmd)
 
                 self.prevStatData[curZone]    = data
-                self.statusDataValid[curZone] = True     
- 
+                self.statusDataValid[curZone] = True
+
 
     def __init__(self, verbose, port, rxCmdQueue, txCmdQueue, run=True):
         threading.Thread.__init__(self)
@@ -289,43 +289,69 @@ class AmpCtrl(threading.Thread):
 
     def close(self):
         self.running = False
-    
+
 
 
 ################################################################################
 if __name__ == "__main__":
-    # get the args 
+    # get the args
     parser = argparse.ArgumentParser(description='Sony AV reciever controller')
-    parser.add_argument('-p', '--port', dest='port', action='store',
+    parser.add_argument('-P', '--port', dest='port', action='store',
                         default='/dev/ttyUSB0', help='Serial port the amp')
     parser.add_argument('-d', '--dump', action="store_true",
                         help='Dumps the internal memory values of the amp')
     parser.add_argument('-a', '--addr', action='store', default='-1',
-                        type=int, help='The address to write to')
-    parser.add_argument('-v', '--val', action='store', default='0',
-                        type=int, help='The value to write')
+                        type=int, help='The memory address to read or write to')
+    parser.add_argument('-v', '--val', action='store', default='-1',
+                        type=int, help='The value to write to a register or to memory. If this argument is not specified the address is read from.')
+    parser.add_argument('-p', '--pdc', action='store', default='-1',
+                        type=int, help='The register PDC to read or write from.')
+    parser.add_argument('-c', '--cmd', action='store', default='-1',
+                        type=int, help='The register command to read or write from.')
+    parser.add_argument('-z', '--zones', action='store', default=None,
+                        type=int, help='The zones to use with a register access')
+
     args = parser.parse_args()
 
     # create the queues we use to connect the different threads
-    rxCmdQueue = Queue.Queue()
-    txCmdQueue = Queue.Queue()
-        
+    rxCmdQueue = queue.Queue()
+    txCmdQueue = queue.Queue()
+
     # new create the components
     amp = AmpCtrl(True, args.port, rxCmdQueue, txCmdQueue, False)
 
-    # is there a command to send
-    if args.addr >= 0:
-        cmd = AmpCmd.AmpMemCmd(args.addr, args.val)
-        amp.sendCmd(cmd)
+    def printMemData(addr, data):
+        # remove the header from the data
+        data = data[6:]
+        for idx in range(0,len(data)):
+            print("addr %d val %d" %(addr+idx,data[idx]))
 
-    # Dump the amp control memory    
+    # if there a register access to perfrom
+    if args.pdc >= 0 and args.cmd >= 0:
+        if args.val >= 0:
+            cmd = AmpCmd(args.pdc, args.cmd,  [args.val], [OK_RESP], args.zones)
+            amp.sendCmd(cmd)
+        else:
+            data = amp.recieveData(args.pdc, args.cmd, [args.zones])
+            print(data)
+
+    # is there a memory access to perform
+    if args.addr >= 0:
+        if args.val >= 0:
+            cmd = AmpCmd.AmpMemCmd(args.addr, args.val)
+            amp.sendCmd(cmd)
+        else:
+            cmd  = [SYSTEM_ADDR_SPACE, args.addr >> 8, args.addr & 0xff, 1]
+            data = amp.recieveData(PDC_AMP, MEM_READ, cmd)
+            printMemData(args.addr, data)
+
+    # Dump the amp control memory
     size = 64
     if args.dump:
         for addr in range(0,3584,size):
-            print "addr %s" %addr 
+            print("\naddr %s" %addr)
             cmd  = [SYSTEM_ADDR_SPACE, addr >> 8, addr & 0xff, size]
             data = amp.recieveData(PDC_AMP, MEM_READ, cmd)
-            for idx in range(0,len(data)):
-                print "addr %d val %d\n" %(addr+idx,data[idx])
+            printMemData(addr, data)
 
     amp.close()
